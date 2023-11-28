3fcdc3fc731c1734bcae02f99cdf73311348b425
Amish Patel <amish920w@gmail.com>
2023-11-27 23:11:07 -0500

    for security of db
    security of db

--- Server/DataBase/db_config.py
+++ Server/DataBase/db_config.py
@@ -1,10 +1,17 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
 
-# MongoDB setup
-uri = "mongodb+srv://amish920w:WZZXSYAlA1hBboq3@clustercheckout.zplphbp.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(uri, server_api=ServerApi('1'))
+# Load environment variables from the .env file
+load_dotenv()
+
+# Retrieve MongoDB URI from environment variables
+mongodb_uri = os.getenv("MONGODB_URI")
+
+# MongoDB setup 
+client = MongoClient(mongodb_uri server_api=ServerApi('1'))
 database = client.get_database("SmartCheckout")
 
 # Create a global variable for the database
 db = database
+
